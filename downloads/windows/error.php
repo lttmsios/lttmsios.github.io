4101de01d79e16c31b52d4e8a45d7839144542ff
beastsmp <125803748+MsiOSLTT@users.noreply.github.com>
2023-02-19 15:41:54 +0200

    V1
    V2

---
+++ downloads/windows/error.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Website is Down</title>
+	<style>
+		/* Center the error message vertically */
+		body {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			height: 100vh;
+		}
+
+		h1 {
+			margin-top: 0;
+		}
+
+		/* Add some padding and border to the error message */
+		.error {
+			padding: 1rem;
+			border: 1px solid #ccc;
+			border-radius: 5px;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<!-- Error message with icon -->
+	<div class="error">
+		<h1><img src="images/error.png" alt="Error Icon" width="64" height="64"></h1>
+		<p>The website is currently down for maintenance. Please try again later or contact support.</p>
+	</div>
+</body>
+</html>
